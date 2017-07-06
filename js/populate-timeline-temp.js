509d40b0e1e6ad950702fe9831747ccfb53c70f3
Gabe <gabriel.philbois@gmail.com>
2017-07-06 14:14:28 -0500

    merged with dev

---
+++ js/populate-timeline-temp.js
@@ -0,0 +1,4 @@
+timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">' + 'I Was Born, But...' + '</a></dt>'
+                        timeline +='<dd class="timeline-event-content" id="event01EX">'
+                               timeline +=' <p>Content about the event goes here.</p>'
+                       timeline +=' </dd>'
