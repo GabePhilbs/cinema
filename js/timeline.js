baef4bb576ab5ebc5a96e80818d4b033d6302f9b
Gabe <gabriel.philbois@gmail.com>
2017-07-06 12:34:34 -0500

    populating but no lnks

--- js/timeline.js
+++ js/timeline.js
@@ -1,133 +1,136 @@
-console.log('Geronimo!!! (calltimeline has been called)');
+// console.log('Geronimo!!! (calltimeline has been called)');
 
 function calltimeline(){    
 	
 	//test if this function is getting called
-	console.log('Geronimo!!! (calltimeline has been called)');
+	//console.log('Geronimo!!! (calltimeline has been called)');
 	//test if films is being loaded
-	console.log(films);
+	//console.log(films);
 
 	var timeline = '<!-- beginning of the navbar block -->'
 
+	
 
 	timeline +='<!-- beginning of timeline -->'
 		timeline +='<div id="timeline" class="timeline-container">'
-		       timeline +=' <div class="timeline-wrapper">'
-		       timeline +="<h2 class='timeline-time'>1930's</h2>"
 
-		             timeline +='<dl class="timeline-series">'
+			for(var i =1930; i <= 2010; i+=10){
+				var n = i.toString();
+		       timeline +=' <div class="timeline-wrapper">'
+		       timeline +="<h2 class='timeline-time '>"+ i +"</h2>"
+
+		             timeline +='<dl class="timeline-series '+ n +'">'
 		          
 						 	
 					   
 
-				             		timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">' + 'I Was Born, But...' + '</a></dt>'
-				                        timeline +='<dd class="timeline-event-content" id="event01EX">'
-				                               timeline +=' <p>Content about the event goes here.</p>'
-				                       timeline +=' </dd>'
-				               
+    			               
 						                  
 
 		                timeline +='</dl>'
 		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>1940's</h2>"
+		    }
 
-		                timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Nora Inu</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                       timeline +=' </dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>1950's</h2>"
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time time-forty'>1940's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Gojira</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                        timeline +='</dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Nora Inu</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                        timeline +=' </dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>1960's</h2>"
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time time-fifty'>1950's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Tokyo Drifter</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                        timeline +='</dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Gojira</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                         timeline +='</dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>1970's</h2>"
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time time-sixty'>1960's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">House</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                        timeline +='</dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Tokyo Drifter</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                         timeline +='</dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>1980's</h2>"
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time'>1970's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Tampopo Juzo Itami</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                        timeline +='</dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">House</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                         timeline +='</dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		               timeline +="<h2 class='timeline-time'>1990's</h2>"
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time'>1980's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Rainy Dog</a></dt>'
-		                       timeline +=' <dd class="timeline-event-content" id="event03EX">'
-		                               timeline +=' <p>Content about the event goes here.</p>'
-		                       timeline +=' </dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Tampopo Juzo Itami</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                         timeline +='</dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>2000's</h2>"
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                timeline +="<h2 class='timeline-time'>1990's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Spirited Away</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                        timeline +='</dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Rainy Dog</a></dt>'
+		//                        timeline +=' <dd class="timeline-event-content" id="event03EX">'
+		//                                timeline +=' <p>Content about the event goes here.</p>'
+		//                        timeline +=' </dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		        timeline +='<div class="timeline-wrapper">'
-		                timeline +="<h2 class='timeline-time'>2010's</h2>"
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time'>2000's</h2>"
 
-		                timeline +='<dl class="timeline-series">'
+		//                 timeline +='<dl class="timeline-series">'
 
-		                        timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = " smoothScroll timelineLink">Outrage</a></dt>'
-		                        timeline +='<dd class="timeline-event-content" id="event03EX">'
-		                                timeline +='<p>Content about the event goes here.</p>'
-		                        timeline +='</dd>'
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">Spirited Away</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                         timeline +='</dd>'
 
-		                timeline +='</dl>'
-		        timeline +='</div>'
-		    timeline +='<br class="clear">'
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//         timeline +='<div class="timeline-wrapper">'
+		//                 timeline +="<h2 class='timeline-time'>2010's</h2>"
+
+		//                 timeline +='<dl class="timeline-series">'
+
+		//                         timeline +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = " smoothScroll timelineLink">Outrage</a></dt>'
+		//                         timeline +='<dd class="timeline-event-content" id="event03EX">'
+		//                                 timeline +='<p>Content about the event goes here.</p>'
+		//                         timeline +='</dd>'
+
+		//                 timeline +='</dl>'
+		//         timeline +='</div>'
+		//     timeline +='<br class="clear">'
 		timeline +='</div>'
-
 
 
 
@@ -143,6 +146,35 @@ function calltimeline(){
  	
 
  	//replace this with .get when the array becomes html
+
+
+
+$.get("http://localhost:8080/films", function(data){
+	films = data;
+	// console.log(films);
+	for(var i =0; i< films.length; i++){
+		//get a decade from the loop
+		var dec = 1930+ (i*10);
+		//turn it to string
+		var n = dec.toString();
+		//crete content to input
+		var insert =''
+	insert +='<dt class="timeline-event" id="event03"><a href="#pageTop" class = "smoothScroll timelineLink">' + films[i].name + '</a></dt>'
+        insert +='<dd class="timeline-event-content" id="event01EX">'
+               insert +=' <p>Content about the event goes here.</p>'
+       insert +=' </dd>'
+
+       console.log(n);
+       // console.log(insert);
+       // console.log(films[i].name);
+       //append to right decade
+       $('.'+ dec).append(insert);
+       }
+
+});
+			
+
+
  	
 $('.timelineLink').click(function(){
 		    filmTitle =($(this).text());
@@ -183,6 +215,10 @@ $('.timelineLink').click(function(){
 
 
 		  });
+
+
+	 	
+	 
 		 
 
 
